d0bcda6f1bc3bb8c311f8668ae26a37f5fcf3098
Alexander Svensson <fakesmilehere@gmail.com>
2018-03-24 12:10:41 +0100

    En massa skit

--- register2.php
+++ register2.php
@@ -1,5 +1,5 @@
 <?php 
-require('../../sql.php');
+require('sql.php');
 $sql = new sql();
 if(isset($_POST['name'])) {
   $name = $_POST['name'];
@@ -37,15 +37,15 @@ if(isset($_POST['name'])) {
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="../../plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -60,12 +60,12 @@ if(isset($_POST['name'])) {
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>KFGF</b>Registrering</a>
+    <a href="index.php"><b>KFGF</b>Registrering</a>
   </div>
   <div class="register-box-body">
     <p class="login-box-msg">Registrera en ny Medlem</p>
 
-    <form action="#" method="post">
+    <form action="register2.php" method="post">
 
       <div class="form-group has-feedback">
         <div class="checkbox icheck">
@@ -148,13 +148,6 @@ if(isset($_POST['name'])) {
       </div>
 
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Användandet av personuppgifter godkänns enligt <a href="#">GDPR</a>
-            </label>
-          </div>
-        </div>
         <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Registrera</button>
@@ -168,11 +161,11 @@ if(isset($_POST['name'])) {
 <!-- /.register-box -->
 
 <!-- jQuery 3 -->
-<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- iCheck -->
-<script src="../../plugins/iCheck/icheck.min.js"></script>
+<script src="plugins/iCheck/icheck.min.js"></script>
 <!-- jquery Mask -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.12/jquery.mask.min.js"></script>
 <!-- Latest compiled and minified CSS -->
